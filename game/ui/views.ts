58f2c5f67843fcf105122e6c4661b5eae17b152b
Jonathan Dupré <hello@jonathandupre.com>
2021-03-18 22:41:57 -0400

    cleaned up views

--- game/ui/views.ts
+++ game/ui/views.ts
@@ -30,21 +30,22 @@ export abstract class View {
   }
 }
 
-export const components: Record<string, UiComponent> = {
-  // top
-  nav: new Navbar(1, 1),
-  // left side
-  profile: new Sidebar(1, 3),
-  stats: new Stats(1, 15),
-  // right side
-  output: new Output(20, 15),
-  prompt: new Input(20, 21),
-};
+export const components: Record<string, UiComponent> = {};
 
+const nav = new Navbar(1, 1);
+const profile = new Sidebar(1, 3);
+const stats = new Stats(1, 15);
+const output = new Output(20, 15);
+const prompt = new Input(20, 21);
 const axis = new Axis(23, 3);
 const room = new RoomMap(axis.position.x + 1, axis.position.y + 1);
 const speech = new Speech(room.position.x, room.position.y);
 
+components.nav = nav;
+components.profile = profile;
+components.stats = stats;
+components.output = output;
+components.prompt = prompt;
 components.axis = axis;
 components.room = room;
 components.speech = speech;
